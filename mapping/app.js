e6394d441090bf055933022bf268b3167b968493
kevinezarizky <45492905+kevinezarizky@users.noreply.github.com>
2021-03-24 21:22:31 +0700

    Mapping beres

--- mapping/app.js
+++ mapping/app.js
@@ -1,91 +1,55 @@
-const ldap = require("ldapjs");
-const express = require("express");
-const app = express();
-const port = 3000;
-let client;
+const express = require('express')
+const app = express()
+const port = 3000
 
-// unbind after completion of process
-function closeConnection() {
-  console.log("closeConnection");
-  client.unbind((err) => {
-    console.log("unbind error", err);
-  });
-}
 
-function search() {
-  const searchOptions = {
-    filter: "(uid=yourSearchText)", // search text
-    scope: "sub",
-  };
-  return new Promise((resolve, reject) => {
-    client.search(
-      "ou=consultants," + 'ou="Your OU",ou=yourOu,dc=yourDc,dc=com',
-      searchOptions,
-      (err, res) => {
-        res.on("searchEntry", (entry) => {
-          console.log("searchEntry", entry.object);
-          resolve(entry.object);
-        });
-        res.on("searchReference", (referral) => {
-          console.log("referral: " + referral.uris.join());
-          resolve(referral.uris.join());
-        });
-        res.on("error", (err) => {
-          console.error("search error: " + err.message);
-          reject(err);
-        });
-        res.on("end", (result) => {
-          console.log("If not found", result);
-          reject({ message: "User not found" });
-        });
-      }
-    );
-  });
-}
+//CODING KEPIN
+const path = require("path")
+const router = express.Router()
+app.set("view engine", "pug");
+app.set("views", path.join('D:/_Bank Indonesia/_CURRENT/learn_express/', "views"));
 
-function authenticate() {
-  const server = "localhost:10389";
-  client = ldap.createClient({
-    url: `ldap://${server}`,
-  });
+app.get('/', (req, res) => {
+	res.render("index");
+})
 
-  return new Promise((resolve, reject) => {
-    client.bind("cn=yourcn,dc=yourdc,dc=com", "sortedSolutions", (err) => {
-      if (err) {
-        reject(err);
-      }
-      resolve("Authenticated successfully");
-    });
-  });
-}
-
-function start(req, res) {
-  let searchResponseData;
-  authenticate()
-    .then((authenticateResponse) => {
-      console.log("authenticateResponse", authenticateResponse);
-      return search();
-    })
-    .then((searchResponse) => {
-      console.log("searchResponsesearchResponse", searchResponse);
-      searchResponseData = searchResponse;
-      return closeConnection();
-    })
-    .then((closeConnectionResponse) => {
-      console.log("ldap connection closed", closeConnectionResponse);
-      res.status(200).send(searchResponseData);
-    })
-    .catch((error) => {
-      console.log("catch error", error);
-      res.status(400).send(error);
-    });
-}
-
-module.exports.start = start;
-app.get("/", (req, res) => {
-  res.send(start("asd", "sad"));
+app.get("/about", (req, res) => {
+	
+	//Ubah Variable nu ieu
+	var id = 'kevin_eza@bi.go.id'
+	var nama = 'Kevin Eza Rizky'
+	var group = 'DKEU'
+	
+	//Variable nu Ditampilkeun
+	var message1 = 'User ID	: '+id;
+	var message2 = 'Nama	: '+nama;
+	var message3 = 'Group	: '+group;
+	var akses_data = [];
+	
+	//Database Mapping
+	var fs  = require("fs");
+	var Data = fs.readFileSync('D:/_Bank Indonesia/_CURRENT/learn_express/views/database.txt').toString().split(';');
+	var database = [];
+	Data.forEach(element => console.log(element));
+	Data.forEach(function(entry) {
+		var temp = entry.split('-');
+		console.log(temp);
+		database.push([[temp[0]],[temp[1]]])
+		}
+	);
+	console.log(database[0][0]);
+	database.forEach(function(pembanding){
+		if(group==pembanding[0]){
+			akses_data.push(pembanding[1])
+		}
+	});
+	
+	//Panggil views
+  res.render("about", { title: "Hey", message1: message1, message2: message2, message3: message3, data:akses_data  });
 });
+
+//END CODING KEPIN
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
